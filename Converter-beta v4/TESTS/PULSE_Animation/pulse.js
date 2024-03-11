f1fde43c06db708f4312758d8086c0d5587390bc
Swareeh <77015406+Swareeh@users.noreply.github.com>
2024-03-11 10:32:35 +0400

    Purge

--- Converter-beta v4/TESTS/PULSE_Animation/pulse.js
+++
@@ -1,9 +0,0 @@
-const mic_btn = document.getElementById('pulse')
-const pulse_spans = document.querySelectorAll('.pulse_span')
-
-mic_btn.addEventListener('click', function () {
-    pulse_spans.forEach((span, index) => {
-        span.style.setProperty('--i', index);
-        span.classList.toggle('listening');
-    });
-})
