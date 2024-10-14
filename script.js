23a4d037b822fef4b785ace630ce09b725b3d569
Swareeh <77015406+Swareeh@users.noreply.github.com>
2024-10-14 21:00:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // alert('Not all versions are functional')
 
 function prototype() {
-    window.location.href = "../Converter-beta/";
+    window.location.href = "../Converter-beta/index.html";
 }
 
 function version_2() {
-    window.location.href = "../Converter-beta%20v2/";
+    window.location.href = "../Converter-beta%20v2/index.html";
 }
 
 function version_3() {
-    window.location.href = "../Converter-beta%20v3/";
+    window.location.href = "../Converter-beta%20v3/index.html";
 }
 
 function version_4() {
-    window.location.href = "https://swareeh.github.io/Speech2Sign/";
+    window.location.href = "https://swareeh.github.io/Speech2Sign/index.html";
 }
