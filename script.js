95ef1c0046030474f3d7937329159f2278e794bc
Swareeh <77015406+Swareeh@users.noreply.github.com>
2024-10-14 21:06:41 +0400

    Update script.js
    why do i keep forgetting things!

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // alert('Not all versions are functional')
 
 function prototype() {
-    window.location.href = "../Converter-beta/index.html";
+    window.location.href = "../speech2sign-beta/Converter-beta/index.html";
 }
 
 function version_2() {
-    window.location.href = "../Converter-beta%20v2/index.html";
+    window.location.href = "../speech2sign-beta/Converter-beta%20v2/index.html";
 }
 
 function version_3() {
-    window.location.href = "../Converter-beta%20v3/index.html";
+    window.location.href = "../speech2sign-beta/Converter-beta%20v3/index.html";
 }
 
 function version_4() {
